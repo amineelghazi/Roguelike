d8652e02a6a9baf6b54d4f43b9e10e29afacd4ef
amineelghazi100 <amineelghazi100@hotmail.com>
2026-09-13 17:24:39 -0400

    Added a FoodPrefab, a Label counting the current amount of food & a function to generate the food on the board

--- Assets/Scripts/BoardManager.cs
+++ Assets/Scripts/BoardManager.cs
@@ -1,5 +1,7 @@
 using UnityEngine;
 using UnityEngine.Tilemaps;
+using System.Collections.Generic;
+
 
 public class BoardManager : MonoBehaviour
 {
@@ -7,12 +9,14 @@ public class BoardManager : MonoBehaviour
     public class CellData
     {
         public bool Passable;
+        public CellObject ContainedObject;
     }
 
 
     private CellData[,] m_BoardData;
     private Tilemap m_Tilemap;
     private Grid m_Grid;
+    private List<Vector2Int> m_EmptyCellsList;
 
 
 
@@ -21,6 +25,7 @@ public class BoardManager : MonoBehaviour
     public Tile[] GroundTiles;
     public Tile[] WallTiles;
     public PlayerController Player;
+    public FoodObject FoodPrefab;
 
 
 
@@ -30,6 +35,8 @@ public class BoardManager : MonoBehaviour
     {
         m_Tilemap = GetComponentInChildren<Tilemap>();
         m_Grid = GetComponentInChildren<Grid>();
+        // Initialize the list
+        m_EmptyCellsList = new List<Vector2Int>();
 
         m_BoardData = new CellData[Width, Height];
 
@@ -49,16 +56,20 @@ public class BoardManager : MonoBehaviour
                 {
                     tile = GroundTiles[Random.Range(0, GroundTiles.Length)];
                     m_BoardData[x, y].Passable = true;
+                    m_EmptyCellsList.Add(new Vector2Int(x, y)); // Add to empty cells list
                 }
 
                 m_Tilemap.SetTile(new Vector3Int(x, y, 0), tile);
             }
         }
+        //remove the starting point of the player! It's not empty, the player is there
+        m_EmptyCellsList.Remove(new Vector2Int(1, 1));
+        GenerateFood();
     }
 
     public Vector3 CellToWorld(Vector2Int cellIndex)
     {
-        return m_Grid.GetCellCenterWorld((Vector3Int)cellIndex); 
+        return m_Grid.GetCellCenterWorld((Vector3Int)cellIndex);
     }
 
     public CellData GetCellData(Vector2Int cellIndex)
@@ -67,6 +78,22 @@ public class BoardManager : MonoBehaviour
         {
             return null;
         }
-        return m_BoardData[cellIndex.x, cellIndex.y]; 
+        return m_BoardData[cellIndex.x, cellIndex.y];
+    }
+
+    void GenerateFood()
+    {
+        int foodCount = 5;
+        for (int i = 0; i < foodCount; i++)
+        {
+            int randomIndex = Random.Range(0, m_EmptyCellsList.Count);
+            Vector2Int coord = m_EmptyCellsList[randomIndex];
+
+            m_EmptyCellsList.RemoveAt(randomIndex); // Remove the cell from the list to avoid placing food there again
+            CellData data = m_BoardData[coord.x, coord.y];
+            FoodObject newFood = Instantiate(FoodPrefab);
+            newFood.transform.position = CellToWorld(coord);
+            data.ContainedObject = newFood;
+        }
     }
 }
--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,12 +1,16 @@
 using UnityEngine;
+using UnityEngine.UIElements;
 
 public class GameManager : MonoBehaviour
 {
     public static GameManager Instance { get; private set; }
     public BoardManager BoardManager;
     public PlayerController PlayerController;
-
     public TurnManager TurnManager { get; private set; }
+    public UIDocument UIDoc;
+
+    private int m_Foodamount = 100;
+    private Label m_FoodLabel;
 
     private void Awake()
     {
@@ -21,9 +25,20 @@ public class GameManager : MonoBehaviour
   
     void Start()
     {
+        m_FoodLabel = UIDoc.rootVisualElement.Q<Label>("FoodLabel");
+        m_FoodLabel.text = "Food: " + m_Foodamount;
+
         TurnManager = new TurnManager();
+        TurnManager.OnTick += OnTurnHappen;
 
         BoardManager.Init();
         PlayerController.Spawn(BoardManager, new Vector2Int(1, 1));
+    }
+
+    void OnTurnHappen()
+    {
+        m_Foodamount--;
+        m_FoodLabel.text = "Food: " + m_Foodamount;
+        Debug.Log("Current amount of food: " + m_Foodamount);
     }
 }
