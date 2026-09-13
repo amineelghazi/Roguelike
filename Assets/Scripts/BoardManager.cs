92331fa38c4c1b364f1afeaa817fb8c30a070c08
amineelghazi100 <amineelghazi100@hotmail.com>
2026-09-13 15:56:56 -0400

    Added a turn system & a GameManager using the singleton pattern

--- Assets/Scripts/BoardManager.cs
+++ Assets/Scripts/BoardManager.cs
@@ -8,42 +8,30 @@ public class BoardManager : MonoBehaviour
     {
         public bool Passable;
     }
+
+
     private CellData[,] m_BoardData;
     private Tilemap m_Tilemap;
     private Grid m_Grid;
+
+
 
     public int Width;
     public int Height;
     public Tile[] GroundTiles;
     public Tile[] WallTiles;
-
     public PlayerController Player;
 
-    public Vector3 CellToWorld(Vector2Int cellIndex)
-    {
-        return m_Grid.GetCellCenterWorld((Vector3Int)cellIndex);
-    }
-    public CellData GetCellData(Vector2Int cellIndex)
-    {
-        if (cellIndex.x < 0 || cellIndex.x >= Width
-            || cellIndex.y < 0 || cellIndex.y >= Height)
-        {
-            return null;
-        }
 
-        return m_BoardData[cellIndex.x, cellIndex.y];
-    }
 
 
     // Start is called before the first frame update
-    void Start()
+    public void Init()
     {
         m_Tilemap = GetComponentInChildren<Tilemap>();
         m_Grid = GetComponentInChildren<Grid>();
 
         m_BoardData = new CellData[Width, Height];
-
-
 
         for (int y = 0; y < Height; ++y)
         {
@@ -66,6 +54,19 @@ public class BoardManager : MonoBehaviour
                 m_Tilemap.SetTile(new Vector3Int(x, y, 0), tile);
             }
         }
-        Player.Spawn(this, new Vector2Int(1, 1));
+    }
+
+    public Vector3 CellToWorld(Vector2Int cellIndex)
+    {
+        return m_Grid.GetCellCenterWorld((Vector3Int)cellIndex); 
+    }
+
+    public CellData GetCellData(Vector2Int cellIndex)
+    {
+        if (cellIndex.x < 0 || cellIndex.x >= Width || cellIndex.y < 0 || cellIndex.y >= Height)
+        {
+            return null;
+        }
+        return m_BoardData[cellIndex.x, cellIndex.y]; 
     }
 }
